4a6b693f31fc708efc51479d36f56d21f0121b57
Tomasz Kasperek <tomasz.kasperek.ext@tipico.com>
2020-02-25 08:17:17 +0100

    initial commit

---
+++ src/main/java/com/example/vhs/VhsApplication.java
@@ -0,0 +1,12 @@
+package com.example.vhs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VhsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VhsApplication.class, args);
+    }
+}
---
+++ src/test/java/com/example/vhs/VhsApplicationTests.java
@@ -0,0 +1,12 @@
+package com.example.vhs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VhsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
